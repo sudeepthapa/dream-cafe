a7d590809ab2ab487a797f635e2096cb17615aea
sudeptech <sudeptech@gmail.com>
2019-04-09 18:32:19 +0545

    dependencies and little animation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,63 @@
-$( function() {
-    $( "#datepicker" ).datepicker();
-    $( "#anim" ).on( "change", function() {
-      $( "#datepicker" ).datepicker( "option", "showAnim", $( this ).val() );
+$(function () {
+    $("#datepicker").datepicker();
+    $("#anim").on("change", function () {
+        $("#datepicker").datepicker("option", "showAnim", $(this).val());
     });
-  } );
+});
+
+AOS.init();
+
+
+
+
+$(window).on("load", function () {
+    anime({
+        targets: '#home .content',
+        translateX: [-400, 0],
+        opacity: [0, 1],
+        easing: 'easeInOutSine'
+    });
+    anime({
+        targets: '#home .image',
+        opacity: [0, 1],
+        easing: 'easeInOutSine',
+        duration: '3000'
+    });
+
+});
+
+
+
+$(document).ready(function(){
+    $('#discover').waypoint(function() {
+        anime({
+            targets: '#discover .images',
+            translateX: [400, 0],
+            opacity: [0, 1],
+            easing: 'easeInOutSine'
+
+        });
+    }, {
+            offset: '95%'
+    });
+    $('#discover').waypoint(function() {
+        anime({
+            targets: '#discover .content',
+            translateX: [-400, 0],
+            opacity: [0, 1],
+            easing: 'easeInOutSine'
+        });
+    }, {
+            offset: '95%'
+    });
+    $('#discover').waypoint(function() {
+        anime({
+            targets: '#discover .content',
+            translateX: [-400, 0],
+            opacity: [0, 1],
+            easing: 'easeInOutSine'
+        });
+    }, {
+            offset: '95%'
+    });
+});
